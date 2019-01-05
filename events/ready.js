325a064fc7a067733774183d60a81bd07ca724b0
yusufcool1 <46259335+yusufcool1@users.noreply.github.com>
2019-01-06 01:39:18 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+   var oyun = [
+        "ÇOK YAKINDA HİZMETİNİZDEYİZ ! ",
+		"ÇOK YAKINDA HİZMETİNİZDEYİZ ! ",
+		"ÇOK YAKINDA HİZMETİNİZDEYİZ ! ",
+		"ÇOK YAKINDA HİZMETİNİZDEYİZ ! ",
+		"ÇOK YAKINDA HİZMETİNİZDEYİZ ! ",
+	    "ÇOK YAKINDA HİZMETİNİZDEYİZ ! "
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/yusuftr_25");
+        }, 2 * 2500);
+}
